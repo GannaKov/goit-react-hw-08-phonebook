9da769d0fecd103e7b88e3f36b9e39bd77e9efbf
GannaKov <zlatta2000@gmail.com>
2023-01-09 20:58:12 +0200

    Loginform, Login page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,12 +11,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout';
 import RegisterPage from '../../pages/Register';
+import LoginPage from '../../pages/Login';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
